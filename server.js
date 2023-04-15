0c9e8fd431f622a50503bdda0270b8fb5c1b4159
Ruturaj <ruturajmengal2016@gmail.com>
2023-04-15 18:16:44 +0530

    add new function

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const router = require("./Routes/router");
-const {errorHandler} = require("./Middleware/middleware");
+const { errorHandler } = require("./Middleware/middleware");
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  res.header({
+  res.set({
     "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*",
+    "Access-Control-Allow-Headers":
+      "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
   });
+
   next();
 });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api", router);
-
-
 
 app.use(errorHandler);
 app.listen(5000, () => {
