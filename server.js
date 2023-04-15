1ba94b3c13e1e6dd447a64440649d58ae187824c
Ruturaj <ruturajmengal2016@gmail.com>
2023-04-15 15:23:10 +0530

    install axios and make a request

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", process.env.URL);
+  res.header({
+    "Access-Control-Allow-Origin": "*",
+  });
   next();
 });
 app.use("/api", router);
