019658c48dcd3d8eb93bea6657c476e227d288f8
Ruturaj <ruturajmengal2016@gmail.com>
2023-04-21 19:49:41 +0530

    changes apply

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const { errorHandler } = require("./Middleware/middleware");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  res.header({
-    "Access-Control-Allow-Origin": "*",
-  });
-
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
-});
+  });
 app.use("/api", router);
 app.use(errorHandler);
 app.listen(5000, () => {
