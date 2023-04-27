67dcc6c5ea4fb93bf37fab15e081b741aa79d108
Ruturaj <ruturajmengal2016@gmail.com>
2023-04-27 21:19:39 +0530

    done

--- Routes/router.js
+++ Routes/router.js
@@ -4,68 +4,66 @@ const router = express.Router();
 const { userLogger } = require("../Middleware/middleware");
 const prisma = new PrismaClient();
 
-router
-  .route("/user")
-  .get(
-    async (req, res, next) => {
-      try {
-        const exist = await prisma.users.findMany({
-          where: {
-            email: req.params.email,
-          },
-        });
-        if (!exist.length) {
-          res.status(404);
-          next(Error("Sorry! You haven't account"));
-        }
-        next();
-      } catch (error) {
-        next(error);
+router.get("/user",async (req, res, next) => {
+    try {
+      const exist = await prisma.users.findMany({
+        where: {
+          email: req.params.email,
+        },
+      });
+      if (!exist.length) {
+        res.status(404);
+        next(Error("Sorry! You haven't account"));
       }
-    },
-    async (req, res, next) => {
-      try {
-        res.status(200);
-        res.send("get successfully...")
-      } catch (error) {
-        next(error);
-      }
+      next();
+    } catch (error) {
+      next(error);
     }
-  )
-  .post(
-    async (req, res, next) => {
-      try {
-        const exist = await prisma.users.findUnique({
-          where: {
-            email: req.body.email,
-          },
-        });
-        if (exist) {
-          res.status(400);
-          next(Error("This email is already used!"));
-        }
-        next();
-      } catch (error) {
-        next(error);
-      }
-    },
-    async (req, res, next) => {
-      try {
-        await prisma.users.create({
-          data: {
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            password: req.body.password,
-          },
-        });
-        res.status(201);
-        res.send("create successfully...");
-      } catch (error) {
+  },async (req, res, next) => {
+    try {
+      res.status(200);
+      res.send("get successfully...");
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post(
+  "/user",
+  async (req, res, next) => {
+    try {
+      const exist = await prisma.users.findUnique({
+        where: {
+          email: req.body.email,
+        },
+      });
+      if (exist) {
         res.status(400);
-        next(error);
+        next(Error("This email is already used!"));
       }
+      next();
+    } catch (error) {
+      next(error);
     }
-  );
+  },
+  async (req, res, next) => {
+    try {
+      await prisma.users.create({
+        data: {
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          email: req.body.email,
+          password: req.body.password,
+        },
+      });
+      res.status(201);
+      res.send("create successfully...");
+    } catch (error) {
+      res.status(400);
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
